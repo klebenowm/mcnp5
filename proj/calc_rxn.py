9b3fdff37acfb38c1d458c7c24287fed8efd00a3
Matthew Klebenow <klebenowm@gmail.com>
2013-05-08 10:48:20 -0500

    Expanded on calc_rx.py script.

--- proj/calc_rxn.py
+++ proj/calc_rxn.py
@@ -1,6 +1,21 @@
 from pyne import transmute
 import numpy
+import sys
 
-with open('mctal','r') as f:
+# First argument is filename (argv[0] is script name)
+filename = sys.argv[1]
+# Read in mctal file
+with open(filename,'r') as f:
     lines = f.readlines()
-phidata = lines[45:89]
+# Define block as the relevant block of text
+block = lines[45:89]
+# Initialize phidata as empty list
+phidata = []
+# Cycle over all lines of the data block
+for line in block:
+    # Split each line into separate elements
+    # and append to list of phidata
+    line = line.split()
+    phidata.append(line)
+# Only retain flux values at even indices (0-indexed)
+phidata = [x for x in phidata if x%2 == 0]
